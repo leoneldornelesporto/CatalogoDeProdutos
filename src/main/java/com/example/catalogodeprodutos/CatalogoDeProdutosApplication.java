47ba89ec4879af34c1af76005fd7359b1cd7d231
Leonel Dorneles Porto <leonel.porto@compasso.com.br>
2021-10-04 14:39:13 -0300

    Initial commit

---
+++ src/main/java/com/example/catalogodeprodutos/CatalogoDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.example.catalogodeprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoDeProdutosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogoDeProdutosApplication.class, args);
+    }
+
+}
